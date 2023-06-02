2b952699aa4dbb6e2e08a32012e83899f51c6d57
Liliyaa11 <120708561+Liliyaa11@users.noreply.github.com>
2023-06-02 11:08:25 +0300

    Add files via upload

---
+++ Migrations/202306020803135_Migration.cs
@@ -0,0 +1,40 @@
+﻿namespace ORM___Magazine.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Brand = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        DateTime = c.Int(nullable: false),
+                        Type = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ProductTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TypeName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ProductTypes");
+            DropTable("dbo.Products");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ORM___Magazine.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ORM___Magazine.Model.StoreDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ORM___Magazine.Model.StoreDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
